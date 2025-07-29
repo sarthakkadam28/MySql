c554aab51736850df5d9dbd4c81c2f0ed9344c08
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-07-29 15:06:36 +0530

    database conectivity with the help of dotnet

---
+++ simpledbsolution/Program.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        string ConnectionString = "Server=localhost; Port=3306; Database=sample; User=root; Password=password";
+
+        using (MySqlConnection con = new MySqlConnection(ConnectionString))
+        {
+            try
+            {
+                con.Open();
+                string strCmd = "SELECT * FROM customers";
+                using (MySqlCommand cmd = new MySqlCommand(strCmd, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"customer_id: {reader["customer_id"]}, name: {reader["name"]}, email: {reader["email"]}, created_at: {reader["created_at"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
